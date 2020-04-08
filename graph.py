bcfdc7c237198d1c6c87fa3eceb0f6a83c398dee
Felix <f.poirier.imt@gmail.com>
2020-04-08 17:05:43 +0200

    Add minimum rating option

--- graph.py
+++ graph.py
@@ -1,13 +1,13 @@
 import pandas as pd
 from settings import file_names
 import networkx as nx
-import community  # run pip install python-louvain
-import json
-import sknetwork as skn
 from itertools import chain
 
 
 def make_friends_graph():
+    """
+    :return: social network graph
+    """
     def get_friends_pairs(user_id, friends):
         return [(user_id, friend) for friend in friends.split(', ')] if type(friends) == str else None
 
@@ -20,11 +20,22 @@ def make_friends_graph():
     return g
 
 
-def make_user_business_bipartite_graph(weighted=True):
+def make_user_business_bipartite_graph(weighted=False, minimum_rating=4):
+    """
+    :param weighted: assign rating as user-business edge weight
+    :param minimum_rating: minimum rating to create user-business edge
+    :return: user-business interaction graph
+    """
+    if minimum_rating <= 5:
+        raise ValueError('Minimum rating must be less than 6')
+
     df = pd.read_csv(file_names['toronto_reviews_without_text'])
+    df = df[df.rating >= minimum_rating]
+
     g = nx.Graph()
     g.add_nodes_from(df.user_id.unique(), bipartite=0)
     g.add_nodes_from(df.business_id.unique(), bipartite=1)
+
     if weighted:
         g.add_weighted_edges_from([(user, business, rating) for user, business, rating
                                in zip(df.user_id, df.business_id, df.rating)])
@@ -34,21 +45,3 @@ def make_user_business_bipartite_graph(weighted=True):
 
     return g
 
-
-def communities_friends(g: nx.Graph): # TODO complete function
-    try:
-        communities = json.load(open(file_names['toronto_communities']))
-    except FileNotFoundError:
-        print('Computing communities with Louvain Algorithm...')
-        communities = community.best_partition(g)  # Calculate partitioning using Louvain Algorithm
-        print('Saving community partition to {}'.format(file_names['toronto_communities']))
-        with open(file_names['toronto_communities'], 'w+') as f:
-            json.dump(communities, fp=f)
-
-        pass
-
-
-def bilouvain_sclustering():  # TODO complete function
-    adjacency_matrix = nx.to_scipy_sparse_matrix(g)
-    bilouvain = skn.clustering.BiLouvain()
-    bilouvain.fit(adjacency_matrix)
