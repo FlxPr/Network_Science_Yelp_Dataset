6ac0386831791c71f57ff85e49762c45108b6703
Felix <f.poirier.imt@gmail.com>
2020-04-09 13:08:54 +0200

    add community detection

--- EDA.py
+++ EDA.py
@@ -11,6 +11,40 @@ import numpy as np
 from collections import Counter
 import networkx as nx
 import powerlaw
+import community
+
+
+def plot_powerlaw_fit(node_degrees):
+    powerlaw_fit = powerlaw.Fit(node_degrees)
+
+    # Plot power law
+    friends_distribution = Counter(node_degrees)
+    fig, ax = plt.subplots()
+    ax.scatter(friends_distribution.keys(), friends_distribution.values())
+
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+    ax.text(0.35, 0.95, 'Power law degree estimate: {:.2f}'.format(powerlaw_fit.power_law.alpha),
+            transform=ax.transAxes, fontsize=14,
+            verticalalignment='top', bbox=props)
+    plt.title('Social network degree distribution')
+    plt.xlabel('Number of friends')
+    plt.ylabel('Count')
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.grid()
+    plt.show()
+
+
+def community_cumulative_plot(communities: dict):
+    community_counts = Counter(communities.values())
+    community_counts = sorted(community_counts.values(), reverse=True)
+    cumulative_community_counts = list(map(lambda x: x/sum(community_counts), np.cumsum(community_counts)))
+    plt.plot(cumulative_community_counts)
+    plt.title('Cumulative community appartenance')
+    plt.grid()
+    plt.xlabel('Number of communities')
+    plt.ylabel('Percentage of users represented')
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -22,23 +56,13 @@ if __name__ == '__main__':
 
     # Fit a power-law to the data
     node_degrees = [node_degree[1] for node_degree in social_net.degree]
-    powerlaw_fit = powerlaw.Fit(node_degrees)
+    plot_powerlaw_fit(node_degrees)
 
-    # Plot power law
-    friends_distribution = Counter(node_degrees)
-    fig, ax = plt.subplots()
-    ax.scatter(friends_distribution.keys(), friends_distribution.values())
-    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-    ax.text(0.35, 0.95, 'Power law degree estimate: {:.2f}'.format(powerlaw_fit.power_law.alpha), transform=ax.transAxes, fontsize=14,
-            verticalalignment='top', bbox=props)
-    plt.title('Social network degree distribution')
-    plt.xlabel('Number of friends')
-    plt.ylabel('Count')
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.grid()
-    plt.show()
-
-
-
-
+    # Use louvain algorithm to maximize modularity in community detection
+    print('Running Louvain algorithm for community detection...')
+    communities = community.best_partition(social_net)
+    modularity = community.modularity(communities, social_net)
+    community_counts = Counter(communities.values())
+    print('Community structure modularity: {:.3f}'.format(modularity))
+    print('Number of communities: {}'.format(len(community_counts)))
+    community_cumulative_plot(communities)
--- graph.py
+++ graph.py
@@ -3,7 +3,6 @@ from settings import file_names
 import networkx as nx
 from itertools import chain
 
-
 def make_friends_graph():
     """
     :return: social network graph
