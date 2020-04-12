792fb9939b5fc66066e71b44b71ff59696b36503
Michael Ullah <ullah.michael@gmail.com>
2020-04-12 16:21:25 +0200

    add igraph builders for bipartite and review+friencds graphs

--- graph.py
+++ graph.py
@@ -5,6 +5,7 @@ from itertools import chain
 import igraph
 import numpy as np
 import itertools
+import os
 
 
 def make_friends_graph(library: str = 'networkx'):
@@ -35,7 +36,7 @@ def make_friends_graph(library: str = 'networkx'):
         raise ValueError('Please use either "networkx" or "igraph" as library')
 
 
-def make_user_business_bipartite_graph(weighted=False, minimum_rating=4):
+def make_user_business_bipartite_graph(weighted=False, minimum_rating=4, igraph_=False):
     """
     :param weighted: assign rating as user-business edge weight
     :param minimum_rating: minimum rating to create user-business edge
@@ -46,6 +47,7 @@ def make_user_business_bipartite_graph(weighted=False, minimum_rating=4):
 
     df = pd.read_csv(file_names['toronto_reviews_without_text'])
     df = df[df.rating >= minimum_rating]
+    
 
     review_network = nx.Graph()
     review_network.add_nodes_from(df.user_id.unique(), bipartite=0)
@@ -58,16 +60,24 @@ def make_user_business_bipartite_graph(weighted=False, minimum_rating=4):
         review_network.add_edges_from([(user, business) for user, business
                                in zip(df.user_id, df.business_id)])
 
+    if igraph_:
+
+        nx.write_graphml(review_network, 'review_network_temporary.graphml')
+        review_network = igraph.read('review_network_temporary.graphml', format='graphml')
+        os.remove("review_network_temporary.graphml")
+    
     return review_network
 
 
-def make_frienships_and_reviews_graph(weight_ratio=1, minimum_rating=0, library: str = 'networkx'):
+def make_frienships_and_reviews_graph(weight_ratio=1, minimum_rating=0, igraph_=False):
     """
     :param weight_ratio: define the ratio of the weights of frienships over reviews. weight_ratio > 1 gives more importance to reviews. 
+    :param minimum_rating: minimum rating to create user-business edge
+    :param igraph: if true, return a graph in igraph library. Otherwise return in networkx library.
     :return: user-user-business interaction graph
     """
-    
-    network = make_friends_graph(library)
+
+    network = make_friends_graph('networkx')
     
     if minimum_rating > 5 :
         raise ValueError('Minimum rating must be less than 6')
@@ -80,6 +90,11 @@ def make_frienships_and_reviews_graph(weight_ratio=1, minimum_rating=0, library:
     network.add_weighted_edges_from([(user, business, rating) for user, business, rating
                             in zip(df.user_id, df.business_id, itertools.repeat(weight_ratio))])
     
+    if igraph_:
+
+        nx.write_graphml(network, 'network_temporary.graphml')
+        network = igraph.read('network_temporary.graphml', format='graphml')
+        os.remove("network_temporary.graphml")
     
     return network
 
